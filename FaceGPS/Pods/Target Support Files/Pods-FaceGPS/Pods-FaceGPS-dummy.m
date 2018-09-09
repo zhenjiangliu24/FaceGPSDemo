99bc9aa22c9f17ac8a10fbb63a2f7234b203578a
Zhenjiang Liu <zliu@kpd-i.com>
2018-09-09 14:29:35 -0400

    add cocoapod and swiftLocation lib

---
+++ FaceGPS/Pods/Target Support Files/Pods-FaceGPS/Pods-FaceGPS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FaceGPS : NSObject
+@end
+@implementation PodsDummy_Pods_FaceGPS
+@end
---
+++ FaceGPS/Pods/Target Support Files/Pods-FaceGPSTests/Pods-FaceGPSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FaceGPSTests : NSObject
+@end
+@implementation PodsDummy_Pods_FaceGPSTests
+@end
